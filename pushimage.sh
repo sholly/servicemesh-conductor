59432e3d5b7fe0d7846a5306808e2b5b79e1fefc
Jay Shollenberger <jshollen@redhat.com>
2021-06-23 08:50:39 -0600

    jaeger works

--- pushimage.sh
+++ pushimage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-docker tag servicemesh-conductor:latest docker.io/sholly/servicemesh-conductor:1.0
-docker push docker.io/sholly/servicemesh-conductor:1.0
+docker tag servicemesh-conductor:latest docker.io/sholly/servicemesh-conductor:jaeger
+docker push docker.io/sholly/servicemesh-conductor:jaeger
